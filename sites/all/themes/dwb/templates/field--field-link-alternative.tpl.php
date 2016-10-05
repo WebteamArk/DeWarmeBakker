d1b26968d478e5ea5b4c19834378e9b5663bd603
Jacek Szmit <jszmit@gmail.com>
2016-10-05 16:31:13 +0200

    theme updates

--- sites/all/themes/dwb/templates/field--field-link-alternative.tpl.php
+++ sites/all/themes/dwb/templates/field--field-link-alternative.tpl.php
@@ -1,4 +1,3 @@
-123
 <div class="<?php print $classes; ?>"<?php print $attributes; ?>>
   <?php if (!$label_hidden): ?>
     <div class="field-label"<?php print $title_attributes; ?>><?php print $label ?>:&nbsp;</div>
