7cf7b12b721586f7719b494b9a0f9f980ccad04b
Jacek Szmit <jszmit@gmail.com>
2016-12-06 11:43:04 +0100

    fix find bakery page

--- sites/all/themes/dwb/js/script.js
+++ sites/all/themes/dwb/js/script.js
@@ -271,6 +271,13 @@ Drupal.behaviors.dwbMultiselect = {
       });
     });
     $('#edit-field-bread-type-tid', context).once('dwb-multiselect').each(function() {
+      var $options = $(this).find('option');
+      $options.detach().sort(function(a,b) {
+          var at = $.trim($(a).text());
+          var bt = $.trim($(b).text());
+          return (at > bt)?1:((at < bt)?-1:0);
+      });
+      $options.appendTo(this);
       $(this).multiselect({
         columns: 1,
         placeholder: Drupal.t('Broodsoort'),
@@ -417,11 +424,36 @@ Drupal.behaviors.dwbFindFix = {
 Drupal.behaviors.dwbSearchInput = {
     attach: function(context, settings) {
       $('.view-find-dwb', context).once('dwb-search-input', function () {
-        $(this).find('#edit-city-wrapper, #edit-distance-wrapper').hide();
+        //$(this).find('#edit-city-wrapper, #edit-distance-wrapper').hide();
+        var $city = $(this).find('#edit-city');
+        var $distance = $(this).find('#edit-distance-postal-code');
+        
         var newInput = document.createElement("input");
         newInput.type="text";
         newInput.id="fakesearch";
+        newInput.placeholder=Drupal.t("Geef een postcode of gemeente in");
         $(this).find('.views-exposed-widgets').prepend(newInput);
+        var $input = $(this).find('#fakesearch');
+        $input.wrap('<div class="fakesearch-wrapper">');
+        $input.val($.trim($city.val() + ' ' + $distance.val()));
+        $input.keyup( function () {
+          var text = $(this).val();
+          var zip = text.match(/\d{4}/g);
+          $city.val(text.replace(/\d{4}/g, ''));
+          if(zip && zip.length) {
+            $distance.val(zip[0]);
+          } else {
+            var loc = $.NpxLoc();
+            $distance.val(loc.npxGetCode($city.val()));
+          }
+        });
+        $input.keypress( function (e) {
+          var keycode = (e.keyCode ? e.keyCode : e.which);
+          if (keycode == '13') {
+            $('button.npxSearchMap', context).click();
+          }
+        });
+        
       });
     },
 };
